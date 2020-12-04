f752ed28cd2575fabd95ee64a489ba3e66d6f4fd
hubin <hubin.android@gmail.com>
2020-12-04 18:11:05 +0800

    国际化；

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:bot_toast/bot_toast.dart';
 import 'package:catcher/catcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:redis_house/generated/l10n.dart';
 import 'package:redis_house/router/application.dart';
 import 'package:redis_house/router/routers.dart';
 
@@ -28,6 +30,13 @@ class MyApp extends StatelessWidget {
       builder: (context, child) {
         return easyLoadingBuilder(context, child);
       },
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       theme: ThemeData(
         primaryColor: Color(0XFF1D2033),
         scaffoldBackgroundColor: Color(0XFF14172A),
