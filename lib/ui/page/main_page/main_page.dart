fffcbc70e06966cdf3e406b38b6f3292c21618a2
hubin <hubin.android@gmail.com>
2020-12-08 23:25:29 +0800

    工地备料；

--- lib/ui/page/main_page/main_page.dart
+++ lib/ui/page/main_page/main_page.dart
@@ -1,9 +1,12 @@
 
+import 'dart:convert';
+
 import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_simple_treeview/flutter_simple_treeview.dart';
 import 'package:hive/hive.dart';
 import 'package:redis_house/ui/page/base_page/base_stateful_state.dart';
 import 'package:split_view/split_view.dart';
@@ -30,12 +33,14 @@ class _MainPageState extends BaseStatefulState<MainPage> {
         initialWeight: 0.2,
         view1: Container(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
                 width: double.infinity,
                 child: FlatButton(
                   color: Color(0x4E80F7),
-                  onPressed: () {
+                  onPressed: () async {
+                    // var box = await Hive.openBox("myBox");
                     var box = Hive.box('myBox');
 
                     box.put('name', 'David');
@@ -47,6 +52,17 @@ class _MainPageState extends BaseStatefulState<MainPage> {
                   child: Text("添加连接", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),),
                 ),
               ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: buildTree(),
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
@@ -56,6 +72,57 @@ class _MainPageState extends BaseStatefulState<MainPage> {
         ),
       ),
     );
+  }
+  var jsonText = '''
+{
+  "employee": {
+    "name": "sonoo",
+    "level": 56,
+    "married": true,
+    "hobby": null
+  },
+  "week": [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ]
+}
+''';
+
+  final TreeController _treeController = TreeController(allNodesExpanded: false);
+  /// Builds tree or error message out of the entered content.
+  Widget buildTree() {
+    try {
+      var parsedJson = json.decode(jsonText);
+      return TreeView(
+        nodes: toTreeNodes(parsedJson),
+        treeController: _treeController,
+      );
+    } on FormatException catch (e) {
+      return Text(e.message);
+    }
+  }
+
+  List<TreeNode> toTreeNodes(dynamic parsedJson) {
+    if (parsedJson is Map<String, dynamic>) {
+      return parsedJson.keys
+          .map((k) => TreeNode(
+          content: Text('$k:'), children: toTreeNodes(parsedJson[k])))
+          .toList();
+    }
+    if (parsedJson is List<dynamic>) {
+      return parsedJson
+          .asMap()
+          .map((i, element) => MapEntry(i,
+          TreeNode(content: Text('[$i]:'), children: toTreeNodes(element))))
+          .values
+          .toList();
+    }
+    return [TreeNode(content: Text(parsedJson.toString()))];
   }
 
 }
