d3af32e53bf76b1007bc2509870b31ae58608b69
hubin <hubin.android@gmail.com>
2020-12-15 18:46:51 +0800

    连接 Redis 并列出 DB；

--- lib/bloc/model/main_page_data.dart
+++ lib/bloc/model/main_page_data.dart
@@ -1,4 +1,5 @@
 
+import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 
 part 'main_page_data.g.dart';
@@ -8,7 +9,22 @@ abstract class MainPageData implements Built<MainPageData, MainPageDataBuilder> 
   @nullable
   bool get connectionListOpen;
 
+  @nullable
+  BuiltMap<String, ConnectionDetail> get connectedRedisMap;
+
   MainPageData._();
   factory MainPageData([updates(MainPageDataBuilder b)]) = _$MainPageData;
 
+}
+
+abstract class ConnectionDetail implements Built<ConnectionDetail, ConnectionDetailBuilder>  {
+
+  @nullable
+  int get dbNum;
+
+  @nullable
+  BuiltMap<String, int> get dbKeyNumMap;
+
+  ConnectionDetail._();
+  factory ConnectionDetail([updates(ConnectionDetailBuilder b)]) = _$ConnectionDetail;
 }
--- lib/ui/page/main_page/main_page.dart
+++ lib/ui/page/main_page/main_page.dart
@@ -3,13 +3,11 @@ import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:getwidget/getwidget.dart';
 import 'package:redis_house/bloc/main_page_bloc.dart';
 import 'package:redis_house/bloc/model/main_page_data.dart';
 import 'package:redis_house/bloc/model/new_connection_data.dart';
 import 'package:redis_house/plugin/redis_plugin/redis.dart';
 import 'package:redis_house/router/application.dart';
-import 'package:redis_house/ui/component/split_view.dart';
 import 'package:redis_house/ui/page/main_page/component/main_page_frame.dart';
 import 'package:redis_house/ui/page/base_page/base_stateful_state.dart';
 import 'package:redis_house/ui/page/main_page/dialog/connection_new_dialog.dart';
@@ -24,142 +22,148 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends BaseStatefulState<MainPage> {
 
-  FocusNode focusNode = FocusNode();
+  FocusNode connectionListFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    super.dispose();
+
+  }
 
   @override
   Widget buildUI(BuildContext context) {
-    return MainPageFrame(
-      topBar: Row(
-        children: [
-          InkWell(
-            onTap: () {
-              newConnectionDialog(context);
-            },
-            hoverColor: Colors.red.withAlpha(128),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.link),
-                  SizedBox(width: 5,),
-                  Text("添加连接")
-                ],
+    return Scaffold(
+      body: MainPageFrame(
+        topBar: Row(
+          children: [
+            InkWell(
+              onTap: () {
+                newConnectionDialog(context);
+              },
+              hoverColor: Colors.red.withAlpha(128),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.link),
+                    SizedBox(width: 5,),
+                    Text("添加连接")
+                  ],
+                ),
               ),
             ),
-          ),
-          SizedBox(width: 50,),
-          InkWell(
-            onTap: () {
-              BotToast.showText(text: "导入");
-            },
-            hoverColor: Colors.red.withAlpha(128),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.get_app),
-                  SizedBox(width: 5,),
-                  Text("导入")
-                ],
+            SizedBox(width: 50,),
+            InkWell(
+              onTap: () {
+                BotToast.showText(text: "导入");
+              },
+              hoverColor: Colors.red.withAlpha(128),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.get_app),
+                    SizedBox(width: 5,),
+                    Text("导入")
+                  ],
+                ),
               ),
             ),
-          ),
-          InkWell(
-            onTap: () {
-              BotToast.showText(text: "导出");
-            },
-            hoverColor: Colors.red.withAlpha(128),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.upload_rounded),
-                  SizedBox(width: 5,),
-                  Text("导出")
-                ],
+            InkWell(
+              onTap: () {
+                BotToast.showText(text: "导出");
+              },
+              hoverColor: Colors.red.withAlpha(128),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.upload_rounded),
+                    SizedBox(width: 5,),
+                    Text("导出")
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-      leftBar: BlocConsumer<MainPageBloc, MainPageData>(
-        listener: (context, state) {
-          if(state.connectionListOpen) {
-            focusNode.requestFocus();
-          } else {
-            focusNode.unfocus();
-          }
-        },
-        builder: (context, state) {
-          return Column(
-            children: [
-              RotatedBox(
-                quarterTurns: -1,
-                child: InkWell(
-                  onTap: () {
-                    bool openState = state.connectionListOpen??false;
-                    context.read<MainPageBloc>().add(ConnectionListOpenEvent(!openState));
-                  },
-                  focusNode: focusNode,
-                  focusColor: Colors.green,
-                  hoverColor: Colors.red.withAlpha(128),
-                  child: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Text("Connection", style: TextStyle(fontSize: 14),),
+          ],
+        ),
+        leftBar: BlocConsumer<MainPageBloc, MainPageData>(
+          listener: (context, state) {
+
+          },
+          builder: (context, state) {
+            return Column(
+              children: [
+                Container(
+                  color: state.connectionListOpen??false ? Colors.green.withAlpha(128) : Colors.transparent,
+                  child: RotatedBox(
+                    quarterTurns: -1,
+                    child: InkWell(
+                      onTap: () {
+                        bool openState = state.connectionListOpen??false;
+                        context.read<MainPageBloc>().add(ConnectionListOpenEvent(!openState));
+                      },
+                      hoverColor: Colors.red.withAlpha(128),
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Text("Connection", style: TextStyle(fontSize: 14),),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          );
-        }
-      ),
-      body: BlocBuilder<MainPageBloc, MainPageData>(
-        builder: (context, state) {
-          return Row(
-            children: [
-              Offstage(
-                offstage: !(state.connectionListOpen??false),
-                child: Container(
-                  width: 300,
-                  color: Colors.black.withAlpha(128),
-                  child: connectionList(),
+              ],
+            );
+          }
+        ),
+        body: BlocBuilder<MainPageBloc, MainPageData>(
+          builder: (context, state) {
+            return Row(
+              children: [
+                Offstage(
+                  offstage: !(state.connectionListOpen??false),
+                  child: Container(
+                    width: 300,
+                    color: Colors.black.withAlpha(128),
+                    child: connectionList(state),
+                  ),
+                ),
+                Expanded(child: Container(
+                  child: Center(child: Text("Redis House.", style: TextStyle(fontSize: 50),)),
+                ))
+              ],
+            );
+          }
+        ),
+        bottomBar: Row(
+          children: [
+            InkWell(
+              onTap: () {
+                BotToast.showText(text: "查看日志");
+              },
+              hoverColor: Colors.red.withAlpha(128),
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.file_present, size: 20,),
+                    SizedBox(width: 5,),
+                    Text("查看日志", style: TextStyle(fontSize: 14),)
+                  ],
                 ),
               ),
-              Expanded(child: Container(
-                child: Center(child: Text("Redis House.", style: TextStyle(fontSize: 50),)),
-              ))
-            ],
-          );
-        }
-      ),
-      bottomBar: Row(
-        children: [
-          InkWell(
-            onTap: () {
-              BotToast.showText(text: "查看日志");
-            },
-            hoverColor: Colors.red.withAlpha(128),
-            child: Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.file_present, size: 20,),
-                  SizedBox(width: 5,),
-                  Text("查看日志", style: TextStyle(fontSize: 14),)
-                ],
-              ),
             ),
-          )
-        ],
+            Expanded(child: Container()),
+          ],
+        ),
       ),
     );
   }
 
-  Widget connectionList() {
+  Widget connectionList(MainPageData mainPageData) {
     return StreamBuilder<List<RecordSnapshot<int, Map>>>(
       stream: intMapStoreFactory.store("t_connection").query().onSnapshots(Application.db),
       builder: (context, snap) {
@@ -168,27 +172,58 @@ class _MainPageState extends BaseStatefulState<MainPage> {
           itemCount: list?.length??0,
           itemBuilder: (context, index) {
             var connection = NewConnectionData.fromJson(list[index].value);
-            return GFAccordion(
-              title: connection.redisName,
-              textStyle: TextStyle(color: Colors.white),
-              contentPadding: EdgeInsets.zero,
-              // titlePadding: EdgeInsets.zero,
-              contentChild: IconButton(icon: Icon(Icons.color_lens), onPressed: (){}),
-              contentBackgroundColor: Colors.transparent,
-              collapsedTitleBackgroundColor: Colors.transparent,
-              expandedTitleBackgroundColor: Colors.transparent,
-            );
-            return ListTile(
-              title: Text(connection.redisName),
-              onTap: () async {
-                bool pingResult = await Redis.instance.connectTo(connection.toJson());
-                BotToast.showText(text: "连接成功");
-              },
-            );
+            var connectionOpened = mainPageData.connectedRedisMap?.containsKey(connection.id)??false;
+            return connectionOpened ? opened(connection, mainPageData) : unOpened(connection, mainPageData);
           },
         );
       },
     );
   }
+
+  Widget unOpened(NewConnectionData connection, MainPageData mainPageData) {
+    return ListTile(
+      onTap: () async {
+        Redis.instance.connectTo(connection.toJson()).then((value) {
+          return Redis.instance.execute(connection.id, "CONFIG GET databases");
+        }).then((value) {
+          context.read<MainPageBloc>().add(ConnectionOpenEvent(connection.id, int.tryParse(value[1])));
+        }).catchError((e) {
+          BotToast.showText(text: "连接出错！");
+        });
+      },
+      title: Text(connection.redisName),
+    );
+  }
+
+  Widget opened(NewConnectionData connection, MainPageData mainPageData) {
+    var connectionDetail = mainPageData.connectedRedisMap[connection.id];
+    return ExpansionTile(
+      title: Row(
+        children: [
+          Icon(Icons.check),
+          SizedBox(width: 5,),
+          Text(connection.redisName),
+        ],
+      ),
+      expandedAlignment: Alignment.topLeft,
+      children: connectionDetail != null ? connectionDetail.dbKeyNumMap.map((key, value) {
+        return MapEntry(
+          key,
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 25, vertical: 8),
+            child: Row(
+              children: [
+                Icon(Icons.landscape),
+                SizedBox(width: 5,),
+                Text(key),
+              ],
+            ),
+          ),
+        );
+      }).values.toList() : [],
+    );
+  }
+
+
 
 }
