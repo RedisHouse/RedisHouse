65c3268bc72633a82e4d08ae8980afcafcef7b1d
hubin <hubin.android@gmail.com>
2020-12-22 16:47:09 +0800

    上下按键导航命令历史；

--- lib/ui/page/main_page/panel/console_panel.dart
+++ lib/ui/page/main_page/panel/console_panel.dart
@@ -26,9 +26,14 @@ class _ConsolePanelState extends State<ConsolePanel> with AfterInitMixin<Console
 
   TextEditingController _textEditingController = TextEditingController();
   FocusNode _focusNode = FocusNode();
+  FocusNode _keyboardFocusNode = FocusNode();
   List<String> commandList = [];
   NewConnectionData connection;
   PanelInfo panelInfo;
+
+  int commandHistoryIndex = 0;
+  List<String> commandHistoryList = [];
+
   @override
   void didInitState() {
     var mainPageBloc = context.read<MainPageBloc>();
@@ -75,63 +80,92 @@ class _ConsolePanelState extends State<ConsolePanel> with AfterInitMixin<Console
   @override
   Widget build(BuildContext context) {
     var dbIndex = StringUtil.isNotBlank(panelInfo.dbIndex) ? panelInfo.dbIndex : "0";
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(commandList.join("\n")),
-          TextField(
-            maxLines: 1,
-            controller: _textEditingController,
-            focusNode: _focusNode,
-            autofocus: true,
-            decoration: InputDecoration(
-                border: InputBorder.none,
-                prefixText: "${connection.redisName}:$dbIndex > "
-            ),
-            onSubmitted: (s) {
-              if(StringUtil.isBlank(s) || s.trim().length == 0) {
-                BotToast.showText(text: "请输入命令");
-                _focusNode.requestFocus();
-                return;
-              }
-              var commandArgList = s.trim().split(" ").where((element) => StringUtil.isNotBlank(element)).toList();
-              Log.d("命令：${commandArgList}");
-              Redis.instance.execute(connection.id, widget.panelUUID, s).then((value) {
-                if(value is List) {
-                  commandList.add("${connection.redisName}:$dbIndex > $s\n${formatListOutput(0, value)}");
-                } else {
-                  commandList.add("${connection.redisName}:$dbIndex > $s\n$value");
+    return RawKeyboardListener(
+      focusNode: _keyboardFocusNode,
+      autofocus: true,
+      onKey: onKeyEvent,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(commandList.join("\n")),
+            TextField(
+              maxLines: 1,
+              controller: _textEditingController,
+              focusNode: _focusNode,
+              autofocus: true,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  prefixText: "${connection.redisName}:$dbIndex > "
+              ),
+              onSubmitted: (s) {
+                if(StringUtil.isBlank(s) || s.trim().length == 0) {
+                  BotToast.showText(text: "请输入命令");
+                  _focusNode.requestFocus();
+                  return;
                 }
-                _textEditingController.clear();
-                if(StringUtil.isEqual(commandArgList[0], "select")) {
-                  int activePanel = context.read<MainPageBloc>().state.activePanelIndex;
-                  Log.d("PanelDBChangeEvent($activePanel, ${commandArgList[1]})");
-                  context.read<MainPageBloc>().add(PanelDBChangeEvent(activePanel, commandArgList[1]));
-                }
-                setState(() {
+                var commandArgList = s.trim().split(" ").where((element) => StringUtil.isNotBlank(element)).toList();
+                Log.d("命令：${commandArgList}");
+                Redis.instance.execute(connection.id, widget.panelUUID, s).then((value) {
+                  addCommandList(s);
+                  if(value is List) {
+                    commandList.add("${connection.redisName}:$dbIndex > $s\n${formatListOutput(0, value)}");
+                  } else {
+                    commandList.add("${connection.redisName}:$dbIndex > $s\n$value");
+                  }
+                  _textEditingController.clear();
+                  if(StringUtil.isEqual(commandArgList[0], "select")) {
+                    int activePanel = context.read<MainPageBloc>().state.activePanelIndex;
+                    Log.d("PanelDBChangeEvent($activePanel, ${commandArgList[1]})");
+                    context.read<MainPageBloc>().add(PanelDBChangeEvent(activePanel, commandArgList[1]));
+                  }
+                  setState(() {
 
+                  });
+                }).catchError((e) {
+                  commandList.add("${connection.redisName}:$dbIndex > $s\n$e");
+                  _textEditingController.clear();
+                  setState(() {
+
+                  });
+                }).whenComplete(() {
+                  _focusNode.requestFocus();
                 });
-              }).catchError((e) {
-                // if(e is PlatformException) {
-                //   Log.d("ExceptionCode: ${e.code}");
-                //   Log.d("ExceptionMessage: ${e.message}");
-                //   Log.d("ExceptionDetails: ${e.details}");
-                // }
-                commandList.add("${connection.redisName}:$dbIndex > $s\n$e");
-                _textEditingController.clear();
-                setState(() {
 
-                });
-              }).whenComplete(() {
-                _focusNode.requestFocus();
-              });
-
-            },
-          )
-        ],
+              },
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  void addCommandList(String command) {
+    bool singleTop = commandHistoryList.isEmpty || StringUtil.isNotEqual(command, commandHistoryList[commandHistoryList.length-1]);
+    if(singleTop) {
+      commandHistoryList.add(command);
+    }
+    commandHistoryIndex = commandHistoryList.length;
+  }
+
+  String previousCommand() {
+    commandHistoryIndex--;
+    if(commandHistoryIndex >= 0) {
+      return commandHistoryList[commandHistoryIndex];
+    } else {
+      commandHistoryIndex = 0;
+      return commandHistoryList[commandHistoryIndex];
+    }
+  }
+
+  String nextCommand() {
+    commandHistoryIndex++;
+    if(commandHistoryIndex < commandHistoryList.length) {
+      return commandHistoryList[commandHistoryIndex];
+    } else {
+      commandHistoryIndex = commandHistoryList.length;
+      return null;
+    }
   }
 
   String formatListOutput(int level, List value) {
@@ -151,6 +185,34 @@ class _ConsolePanelState extends State<ConsolePanel> with AfterInitMixin<Console
       stringBuffer.write("\n");
     }
     return stringBuffer.toString();
+  }
+
+  void onKeyEvent(RawKeyEvent event) {
+    switch (event.runtimeType) {
+      case RawKeyDownEvent:
+        // 按下
+        break;
+      case RawKeyUpEvent:
+        // 松开
+        if(StringUtil.isEqual("Arrow Up", event.physicalKey.debugName)) {
+          String command = previousCommand();
+          _textEditingController.clear();
+          if(StringUtil.isNotBlank(command)) {
+            // _textEditingController.text = command;
+            _textEditingController.value = TextEditingValue(text: command, selection: TextSelection.collapsed(offset: command.length));
+          }
+        } else if(StringUtil.isEqual("Arrow Down", event.physicalKey.debugName)) {
+          String command = nextCommand();
+          _textEditingController.clear();
+          if(StringUtil.isNotBlank(command)) {
+            // _textEditingController.text = command;
+            _textEditingController.value = TextEditingValue(text: command, selection: TextSelection.collapsed(offset: command.length));
+          }
+        }
+        break;
+      default:
+        break;
+    }
   }
 
 }
